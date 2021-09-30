629b7ad91fd55f2cbb716165bcdfe0158ac6b666
Selorm justice Bruce <JusticeSelormBruce@gmail.com>
2021-09-30 01:07:14 -0700

    initial commit

---
+++ src/Components/Section.js
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import styled  from 'styled-components'
+function Section() {
+    return (
+        <Wrap>
+         
+        </Wrap>
+    )
+}
+
+export default Section
+const Wrap = styled.div
+`width:100vw;
+ height:100vh;
+ background-image:url('/images/model-s.jpg');
+`
