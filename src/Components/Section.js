11d9ec2a0d6cdfe026a461d73927695d2fe51444
Selorm justice Bruce <JusticeSelormBruce@gmail.com>
2021-10-16 15:25:27 -0700

    Design Home Page

--- src/Components/Section.js
+++ src/Components/Section.js
@@ -1,17 +1,74 @@
 
 import React from 'react'
-import styled  from 'styled-components'
+import styled from 'styled-components'
 function Section() {
     return (
         <Wrap>
-         
+            <ItemText>
+                <h1>Model S</h1>
+                <p>Order  Online for Touchless Delivery</p>
+            </ItemText>
+            <Buttons>
+            <ButtonGroup>
+                <LeftButton>
+                    Custom Order
+                </LeftButton>
+                <RightButton>
+                    Existing Inventory
+                </RightButton>
+            </ButtonGroup>
+            <DownArrow src="/images/down-arrow.svg" />
+            </Buttons>
+
         </Wrap>
     )
 }
 
 export default Section
-const Wrap = styled.div
-`width:100vw;
- height:100vh;
- background-image:url('/images/model-s.jpg');
-`
+    const Wrap = styled.div
+    `width:100vw;
+    height:100vh;
+    background-size:cover;
+    background-position:center;
+    background-repeat:no-repeat;
+    background-image:url('/images/model-s.jpg');
+    display:flex;
+    flex-direction:column;
+    justify-content:space-between;
+    align-items:center;
+    `
+
+    const ItemText = styled.div`
+    padding-top:15vh;
+    text-align:center;
+    `
+
+    const ButtonGroup = styled.div`
+    display:flex; 
+    margin-button:30px;
+
+    `
+    const LeftButton = styled.div`
+    background-color: rgba(23,26,32,0.8);
+    height:40px;
+    width:256px;
+    color:white;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    border-radius:100px;
+    opacity:0.8;
+    text-transform:uppercase;
+    font-size:12px;
+    cursor: pointer;
+    margin:8px;
+    `
+    const RightButton = styled(LeftButton)`
+    `
+    const DownArrow =styled.img`
+    margin-top:20px;
+    height:40px;
+    animation: animateDown infinite 1.5s;
+    `
+    const Buttons = styled.div`
+    `
